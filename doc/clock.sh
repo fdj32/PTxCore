06b35b1558b7575722b8017fb4186af220025c8d
Nick Feng <fdj32@163.com>
2016-10-25 11:00:16 +0800

    add clock.sh script

---
+++ doc/clock.sh
@@ -0,0 +1,9 @@
+
+CLASSPATH=$CLASSPATH:../bin
+CLASSPATH=$CLASSPATH:../lib/comm.jar
+CLASSPATH=$CLASSPATH:../lib/commons-codec-1.4.jar
+CLASSPATH=$CLASSPATH:../lib/commons-io-1.4.jar
+CLASSPATH=$CLASSPATH:../lib/commons-lang-2.4.jar
+CLASSPATH=$CLASSPATH:../lib/RXTXcomm.jar
+
+/home/nfeng/jdk1.8.0_74/bin/java -classpath $CLASSPATH serial.Clock
