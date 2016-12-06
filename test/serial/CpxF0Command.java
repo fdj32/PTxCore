c6c5abb7cee62b03a086236ca5cf1c20ef05bc5e
Nick Feng <fdj32@163.com>
2016-12-06 14:46:14 +0800

    add F0 Commands

--- test/serial/CpxF0Command.java
+++ test/serial/CpxF0Command.java
@@ -59,10 +59,10 @@ public class CpxF0Command {
 	}
 
 	public void setTo(int to) {
-		if(to > 75){
+		if (to > 75) {
 			to = 75;
 		}
-		if(to < 0) {
+		if (to < 0) {
 			to = 0;
 		}
 		setTo(new String(new byte[] { (byte) (to * 100 / 0x100),
@@ -104,12 +104,220 @@ public class CpxF0Command {
 	 * @return
 	 */
 	public static CpxF0Command cpxF0MsrRead(int to, int cmd) {
-		CpxF0Command msrRead = new CpxF0Command();
-		msrRead.setLgt(new String(new byte[] { 0, 0x04 }));
-		msrRead.setType(new String(new byte[] { 0x04 }));
-		msrRead.setTo(to);
-		msrRead.setCmd(new String(new byte[] { (byte) (cmd) }));
-		return msrRead;
+		CpxF0Command c = new CpxF0Command();
+		c.setLgt(new String(new byte[] { 0, 0x04 }));
+		c.setType(new String(new byte[] { 0x04 }));
+		c.setTo(to);
+		c.setCmd(new String(new byte[] { (byte) (cmd) }));
+		return c;
+	}
+
+	/**
+	 * CPX+EMV+Emulation.pdf Page.79/184
+	 * 
+	 * @param to
+	 *            timeout in seconds
+	 * @return
+	 */
+	public static CpxF0Command cpxF0CancelMsrRead(int to) {
+		CpxF0Command c = new CpxF0Command();
+		c.setLgt(new String(new byte[] { 0, 0x04 }));
+		c.setType(new String(new byte[] { 0x04 }));
+		c.setTo(to);
+		c.setCmd(new String(new byte[] { (byte) (0x12) }));
+		return c;
+	}
+
+	/**
+	 * CPX+EMV+Emulation.pdf Page.84/184
+	 * 
+	 * @param to
+	 *            timeout in seconds
+	 * @return
+	 */
+	public static CpxF0Command cpxF0DefineRemoveCardPrompt(int to) {
+		CpxF0Command c = new CpxF0Command();
+		c.setLgt(new String(new byte[] { 0, 0x04 }));
+		c.setType(new String(new byte[] { 0x05 }));
+		c.setTo(to);
+		// 0x10 = Remove card prompt
+		c.setCmd(new String(new byte[] { (byte) (0x10) }));
+		return c;
+	}
+
+	/**
+	 * CPX+EMV+Emulation.pdf Page.84/184
+	 * 
+	 * @param to
+	 *            timeout in seconds
+	 * @return
+	 */
+	public static CpxF0Command cpxF0MSRwithSCDetectCancel(int to) {
+		CpxF0Command c = new CpxF0Command();
+		c.setLgt(new String(new byte[] { 0, 0x04 }));
+		c.setType(new String(new byte[] { 0x05 }));
+		c.setTo(to);
+		// 0x12 = cancel
+		c.setCmd(new String(new byte[] { (byte) (0x12) }));
+		return c;
+	}
+
+	/**
+	 * CPX+EMV+Emulation.pdf Page.86/184
+	 * 
+	 * @param to
+	 *            timeout in seconds
+	 * @return
+	 */
+	public static CpxF0Command cpxF0WaitForSmartCardInsertion(int to) {
+		CpxF0Command c = new CpxF0Command();
+		c.setLgt(new String(new byte[] { 0, 0x04 }));
+		c.setType(new String(new byte[] { 0x07 }));
+		c.setTo(to);
+		// 0x04: Waiting for a smart card present in the connector
+		c.setCmd(new String(new byte[] { (byte) (0x04) }));
+		return c;
+	}
+
+	/**
+	 * CPX+EMV+Emulation.pdf Page.87/184
+	 * 
+	 * @param to
+	 *            timeout in seconds
+	 * @return
+	 */
+	public static CpxF0Command cpxF0PowerUpCard(int to) {
+		CpxF0Command c = new CpxF0Command();
+		c.setLgt(new String(new byte[] { 0, 0x04 }));
+		c.setType(new String(new byte[] { 0x07 }));
+		c.setTo(to);
+		// 0x05 : Smart card ATR sequence for asynchronous cards or recognition
+		// of synchronous card
+		c.setCmd(new String(new byte[] { (byte) (0x05) }));
+		return c;
+	}
+
+	/**
+	 * CPX+EMV+Emulation.pdf Page.88/184
+	 * 
+	 * @param to
+	 *            timeout in seconds
+	 * @return
+	 */
+	public static CpxF0Command cpxF0WaitInsertAndPowerUp(int to) {
+		CpxF0Command c = new CpxF0Command();
+		c.setLgt(new String(new byte[] { 0, 0x04 }));
+		c.setType(new String(new byte[] { 0x07 }));
+		c.setTo(to);
+		// 0x07 : Smart card ATR sequence for asynchronous cards or recognition
+		// of synchronous card = command 0x04 followed by command 0x05
+		c.setCmd(new String(new byte[] { (byte) (0x07) }));
+		return c;
+	}
+
+	/**
+	 * CPX+EMV+Emulation.pdf Page.89/184
+	 * 
+	 * @param to
+	 *            timeout in seconds
+	 * @return
+	 */
+	public static CpxF0Command cpxF0PowerUpCardAndControlsForATR(int to) {
+		CpxF0Command c = new CpxF0Command();
+		c.setLgt(new String(new byte[] { 0, 0x04 }));
+		c.setType(new String(new byte[] { 0x07 }));
+		c.setTo(to);
+		// 0x08 : command 0x04 and 0x05 with comparison of the ATR received in
+		// commands 0x05 and this command
+		c.setCmd(new String(new byte[] { (byte) (0x08) }));
+		return c;
+	}
+
+	/**
+	 * CPX+EMV+Emulation.pdf Page.90/184
+	 * 
+	 * @param to
+	 *            timeout in seconds
+	 * @return
+	 */
+	public static CpxF0Command cpxF0PowerOffCard(int to) {
+		CpxF0Command c = new CpxF0Command();
+		c.setLgt(new String(new byte[] { 0, 0x04 }));
+		c.setType(new String(new byte[] { 0x07 }));
+		c.setTo(to);
+		// 0x0A smart card power off
+		c.setCmd(new String(new byte[] { (byte) (0x0A) }));
+		return c;
+	}
+
+	/**
+	 * CPX+EMV+Emulation.pdf Page.91/184
+	 * 
+	 * @param to
+	 *            timeout in seconds
+	 * @return
+	 */
+	public static CpxF0Command cpxF0WaitForRemovalOfCard(int to) {
+		CpxF0Command c = new CpxF0Command();
+		c.setLgt(new String(new byte[] { 0, 0x04 }));
+		c.setType(new String(new byte[] { 0x07 }));
+		c.setTo(to);
+		// 0x0B waiting for removal of smart card
+		c.setCmd(new String(new byte[] { (byte) (0x0B) }));
+		return c;
+	}
+
+	/**
+	 * CPX+EMV+Emulation.pdf Page.92/184
+	 * 
+	 * @param to
+	 *            timeout in seconds
+	 * @return
+	 */
+	public static CpxF0Command cpxF0PowerOffCardAndWaitForSmartCardRemoval(
+			int to) {
+		CpxF0Command c = new CpxF0Command();
+		c.setLgt(new String(new byte[] { 0, 0x04 }));
+		c.setType(new String(new byte[] { 0x07 }));
+		c.setTo(to);
+		// 0x0C smart card power off and waiting for card removal command 0x0A
+		// followed by command 0x0B
+		c.setCmd(new String(new byte[] { (byte) (0x0C) }));
+		return c;
+	}
+
+	/**
+	 * CPX+EMV+Emulation.pdf Page.97/184
+	 * 
+	 * @param to
+	 *            timeout in seconds
+	 * @return
+	 */
+	public static CpxF0Command cpxF0SmartCardCommandCancel(int to) {
+		CpxF0Command c = new CpxF0Command();
+		c.setLgt(new String(new byte[] { 0, 0x04 }));
+		c.setType(new String(new byte[] { 0x07 }));
+		c.setTo(to);
+		// 0x12 = cancel
+		c.setCmd(new String(new byte[] { (byte) (0x12) }));
+		return c;
+	}
+
+	/**
+	 * CPX+EMV+Emulation.pdf Page.97/184
+	 * 
+	 * @param to
+	 *            timeout in seconds
+	 * @return
+	 */
+	public static CpxF0Command cpxF0SmartCardStatus(int to) {
+		CpxF0Command c = new CpxF0Command();
+		c.setLgt(new String(new byte[] { 0, 0x04 }));
+		c.setType(new String(new byte[] { 0x07 }));
+		c.setTo(to);
+		// 0x12 = cancel
+		c.setCmd(new String(new byte[] { (byte) (0x18) }));
+		return c;
 	}
 
 }
