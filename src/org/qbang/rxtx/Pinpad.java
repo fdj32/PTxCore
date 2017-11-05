3671573792d8045a8114e52eabf0d433e0af1783
nfeng <nfeng@nfeng-All-Series>
2017-11-05 16:24:58 +0800

    write packets

--- src/org/qbang/rxtx/Pinpad.java
+++ src/org/qbang/rxtx/Pinpad.java
@@ -8,6 +8,7 @@ import gnu.io.CommPortIdentifier;
 import gnu.io.PortInUseException;
 import gnu.io.SerialPort;
 import serial.CpxF1Command;
+import serial.CpxF1Request;
 import serial.UTFUtils;
 import serial.VegaEmvInitReq;
 
@@ -29,11 +30,11 @@ public class Pinpad {
 	private static final int SERIAL_PORT_OPEN_TIMEOUT = 2000;
 	private static final int SERIAL_PORT_READ_TIMEOUT = 8000;
 	private static final int SERIAL_PORT_POLL_TIME = 100;
+	private static final int MAX_TRY_SEND_TIMES = 3;
 	private static final String SERIAL_PORT_NAME = "/dev/tty.usbmodem1411";
 	private static final int MAX_VEGA_PACKET_SIZE = 498;
 	
 	private static int cpxSeqId = 0;
-
 	private SerialPort serialPort;
 	private ConcurrentLinkedQueue<byte[]> dataQ = new ConcurrentLinkedQueue<byte[]>();
 
@@ -89,7 +90,14 @@ public class Pinpad {
 	
 	public boolean cpxF1AsyncEmvData(VegaEmvInitReq initReq, boolean waitForResponse) {
 		CpxF1Command cmd = CpxF1Command.cpxF1AsyncEmvData((byte) cpxSeqId, initReq.toBinary());
-		
+		CpxF1Request req = new CpxF1Request(cmd);
+		byte[] data = req.toBinary();
+		byte[] resp = null;
+		try {
+			resp = write(data, true, MAX_TRY_SEND_TIMES);
+		} catch (Exception e) {
+			return false;
+		}
 		
 		
 		// TODO
