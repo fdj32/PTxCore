3f327be31580712411cf680bc4603bcb198e6247
nickfeng <nickfeng@10.110.122.176>
2017-11-07 17:08:21 +0800

    ExtenedAPIData.toBinary

--- test/serial/ExtendedAPIData.java
+++ test/serial/ExtendedAPIData.java
@@ -15,35 +15,76 @@ import serial.enums.Tag;
  * @author nickfeng
  *
  */
-public class ExtendedAPIData {
+public class ExtendedAPIData implements Constant {
 
 	private byte[] lengthStepTags;
 	private Map<Integer, Map<Integer, List<Tag>>> tagListToAskFor;
 	private Map<Integer, Map<Integer, List<Tag>>> tagListInCallBack;
+
+	public byte[] getLengthStepTags() {
+		return lengthStepTags;
+	}
+
+	public void setLengthStepTags(byte[] lengthStepTags) {
+		this.lengthStepTags = lengthStepTags;
+	}
+
+	public Map<Integer, Map<Integer, List<Tag>>> getTagListToAskFor() {
+		return tagListToAskFor;
+	}
+
+	public void setTagListToAskFor(Map<Integer, Map<Integer, List<Tag>>> tagListToAskFor) {
+		this.tagListToAskFor = tagListToAskFor;
+	}
+
+	public Map<Integer, Map<Integer, List<Tag>>> getTagListInCallBack() {
+		return tagListInCallBack;
+	}
+
+	public void setTagListInCallBack(Map<Integer, Map<Integer, List<Tag>>> tagListInCallBack) {
+		this.tagListInCallBack = tagListInCallBack;
+	}
 
 	public byte[] toBinary() throws IOException {
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		ByteArrayOutputStream baosTemp = new ByteArrayOutputStream();
 		for (int i = EmvTransactionType.EMV_PURCHASE; i <= EmvTransactionType.EMV_PREAUTH_COMPLETION; i++) {
 			for (int j = EmvTransactionStep.EMV_LANGUAGE_SELECTION; j <= EmvTransactionStep.EMV_TRANSACTION_COMPLETION; j++) {
-				for (Tag item : tagListToAskFor.get(i).get(j)) {
-					baosTemp.write(item.getIdBin());
+				baos.write(RFU);
+				baos.write(RFU);
+				if(null == tagListToAskFor || null == tagListToAskFor.get(i)
+						|| 0 == tagListToAskFor.get(i).size()
+						|| null == tagListToAskFor.get(i).get(j)
+						|| 0 == tagListToAskFor.get(i).get(j).size()) {
+					baos.write(0x00);
+					baos.write(0x00);
+				} else {
+					for (Tag item : tagListToAskFor.get(i).get(j)) {
+						baosTemp.write(item.getIdBin());
+					}
+					byte[] tagListToAskForBin = baosTemp.toByteArray();
+					baosTemp.reset();
+					byte[] lenTagListToAskFor = UTFUtils.lgt(tagListToAskForBin.length, 2);
+					baos.write(lenTagListToAskFor);
+					baos.write(tagListToAskForBin);
 				}
-				byte[] tagListToAskForBin = baosTemp.toByteArray();
-				baosTemp.reset();
-				byte[] lenTagListToAskFor = UTFUtils.lgt(tagListToAskForBin.length, 2);
-
-				for (Tag item : tagListInCallBack.get(i).get(j)) {
-					baosTemp.write(item.getIdBin());
+				
+				if(null == tagListInCallBack || null == tagListInCallBack.get(i)
+						|| 0 == tagListInCallBack.get(i).size()
+						|| null == tagListInCallBack.get(i).get(j)
+						|| 0 == tagListInCallBack.get(i).get(j).size()) {
+					baos.write(0x00);
+					baos.write(0x00);
+				} else {
+					for (Tag item : tagListInCallBack.get(i).get(j)) {
+						baosTemp.write(item.getIdBin());
+					}
+					byte[] tagListInCallBackBin = baosTemp.toByteArray();
+					baosTemp.reset();
+					byte[] lenTagListInCallBack = UTFUtils.lgt(tagListInCallBackBin.length, 2);
+					baos.write(lenTagListInCallBack);
+					baos.write(tagListInCallBackBin);
 				}
-				byte[] tagListInCallBackBin = baosTemp.toByteArray();
-				baosTemp.reset();
-				byte[] lenTagListInCallBack = UTFUtils.lgt(tagListInCallBackBin.length, 2);
-
-				baos.write(lenTagListToAskFor);
-				baos.write(tagListToAskForBin);
-				baos.write(lenTagListInCallBack);
-				baos.write(tagListInCallBackBin);
 			}
 		}
 		byte[] data = baos.toByteArray();
@@ -57,9 +98,10 @@ public class ExtendedAPIData {
 		baos.close();
 		return data;
 	}
-	
+
 	public static ExtendedAPIData fromBinary(byte[] bin) {
 		ExtendedAPIData e = new ExtendedAPIData();
+
 		return e;
 	}
 
