989bbfcb7015e2914988e3df5fc6dc22150ae81e
Nick Feng <fdj32@163.com>
2017-11-11 18:51:05 +0800

    AIDToBin is ok

--- doc/ipp320/src/object.c
+++ doc/ipp320/src/object.c
@@ -14,6 +14,28 @@ int AIDLength(AID * o) {
 char * AIDToBin(AID * o) {
 	int length = AIDLength(o);
 	char * s = malloc(length);
+	memset(s, 0, length);
+	memcpy(s, o->applicationSelectionIndicator, 1);
+	int tlv = o->lengthTLVData;
+	memcpy(s[1], tlv, 1);
+	memcpy(s[2], o->tlvData, tlv);
+	memcpy(s[2+tlv], o->aidLength, 1);
+	memcpy(s[3+tlv], o->aid, o->aidLength); // 16
+	memcpy(s[19+tlv], o->rid, 5);
+	memcpy(s[24+tlv], o->applicationVersionNumber, 2);
+	memcpy(s[26+tlv], o->tacDefault, 5);
+	memcpy(s[31+tlv], o->tacDenial, 5);
+	memcpy(s[36+tlv], o->tacOnline, 5);
+	memcpy(s[41+tlv], o->maximumTargetPercentage, 1);
+	memcpy(s[42+tlv], o->targetPercentage, 1);
+	memcpy(s[43+tlv], o->thresholdValue, 4);
+	memcpy(s[47+tlv], o->terminalFloorLimit, 4);
+	memcpy(s[51+tlv], o->defaultTDOLLength, 2);
+	int tdol = littleEndianInt(o->defaultTDOLLength);
+	memcpy(s[53+tlv], o->defaultTDOL, tdol);
+	int ddol = littleEndianInt(o->defaultDDOLLength);
+	memcpy(s[53+tlv+tdol], o->defaultDDOLLength, 2);
+	memcpy(s[55+tlv+tdol], o->defaultDDOL, ddol);
 	return s;
 }
 
