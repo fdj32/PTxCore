1904c9484429e0bfa8c8075e35d9eeb119cbe419
nickfeng <nickfeng@10.110.123.107>
2018-03-16 15:35:14 +0800

    Greenhouse-South-BigBatchMessageSpecs-Nov2017.pdf PURCHASE CARD LEVEL III ADDITIONAL DATA REQUEST RECORD 2, TYPE P2 61/166

---
+++ src/com/globalpaymentsinc/greenhouse/Lever3P2.java
@@ -0,0 +1,133 @@
+package com.globalpaymentsinc.greenhouse;
+
+import org.apache.commons.lang.StringUtils;
+
+/**
+ * PURCHASE CARD LEVEL III ADDITIONAL DATA REQUEST RECORD 2, TYPE P2
+ * 
+ * @author nickfeng
+ *
+ */
+public class Lever3P2 {
+	/**
+	 * Record Type, 1-2, AN 2, Always P2.
+	 */
+	private String recordType = "P2";
+
+	/**
+	 * Reference Number, 3-27, AN 25
+	 * <ul>
+	 * <li>For Visa: The Unique VAT Invoice Reference Number. The value is limited
+	 * to 15 characters.</li>
+	 * <li>For MasterCard: The Card Acceptor Reference Number.</li>
+	 * <li>Space - Not applicable</li>
+	 * </ul>
+	 */
+	private String referenceNumber;
+
+	/**
+	 * National Tax, 28-39, N 12, For Visa only: The national tax amount. 2 decimal
+	 * places assumed, right justified, zero filled. Amount cannot exceed 99,999.99.
+	 */
+	private String nationalTax;
+
+	/**
+	 * Other Tax, 40-51, N 12, For Visa only: The amount of any other tax included
+	 * in the transaction amount. 2 decimal places assumed, right justified, zero
+	 * filled. Amount cannot exceed 99,999.99.
+	 */
+	private String otherTax;
+
+	/**
+	 * VAT / Tax Amount (Freight / Shipping), 52-63, N 12, For Visa only: The tax
+	 * amount applied to freight / shipping costs only. 2 decimal places assumed,
+	 * right justified, zero filled. Amount cannot exceed 99,999.99.
+	 */
+	private String vatAmount;
+
+	/**
+	 * VAT / Tax Rate (Freight / Shipping), 64-67, N 4, For Visa only: The tax rate
+	 * applied to freight / shipping costs only. 2 decimal places assumed, right
+	 * justified, zero filled.
+	 */
+	private String vatRate;
+
+	/* Filler, 68-74, AN 7, Space Filled. */
+
+	/* Reserved, 75-80, AN 6, Space Filled. */
+
+	public String getRecordType() {
+		return recordType;
+	}
+
+	public void setRecordType(String recordType) {
+		this.recordType = recordType;
+	}
+
+	public String getReferenceNumber() {
+		return referenceNumber;
+	}
+
+	public void setReferenceNumber(String referenceNumber) {
+		this.referenceNumber = referenceNumber;
+	}
+
+	public String getNationalTax() {
+		return nationalTax;
+	}
+
+	public void setNationalTax(String nationalTax) {
+		this.nationalTax = nationalTax;
+	}
+
+	public String getOtherTax() {
+		return otherTax;
+	}
+
+	public void setOtherTax(String otherTax) {
+		this.otherTax = otherTax;
+	}
+
+	public String getVatAmount() {
+		return vatAmount;
+	}
+
+	public void setVatAmount(String vatAmount) {
+		this.vatAmount = vatAmount;
+	}
+
+	public String getVatRate() {
+		return vatRate;
+	}
+
+	public void setVatRate(String vatRate) {
+		this.vatRate = vatRate;
+	}
+
+	@Override
+	public String toString() {
+		StringBuffer sb = new StringBuffer();
+		sb.append(recordType);
+		sb.append(referenceNumber);
+		sb.append(StringUtils.leftPad(nationalTax, 12, '0'));
+		sb.append(StringUtils.leftPad(otherTax, 12, '0'));
+		sb.append(StringUtils.leftPad(vatAmount, 12, '0'));
+		sb.append(StringUtils.leftPad(vatRate, 4, '0'));
+		sb.append(StringUtils.repeat(" ", 13));
+		return sb.toString();
+	}
+
+	public static Lever3P2 fromString(String s) {
+		if (StringUtils.isEmpty(s) || s.length() != 80 || !s.startsWith("P2")) {
+			return null;
+		}
+		Lever3P2 p2 = new Lever3P2();
+		p2.setReferenceNumber(s.substring(2, 27));
+		p2.setNationalTax(s.substring(27, 39));
+		p2.setOtherTax(s.substring(39, 51));
+		p2.setVatAmount(s.substring(51, 63));
+		p2.setVatRate(s.substring(63, 67));
+		return p2;
+	}
+
+}
