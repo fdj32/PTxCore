547a7009d657a730cf113bafbdabb5beded00bec
nickfeng <nickfeng@10.110.123.2>
2018-03-20 09:51:37 +0800

    Greenhouse-South-BigBatchMessageSpecs-Nov2017.pdf MARKET DATA ADDENDUM EXTENSION RECORD, TYPE 10 95/166

--- src/com/globalpaymentsinc/greenhouse/BMarketDataAddendumExtension10.java
+++ src/com/globalpaymentsinc/greenhouse/BMarketDataAddendumExtension10.java
@@ -3,25 +3,27 @@ package com.globalpaymentsinc.greenhouse;
 import org.apache.commons.lang.StringUtils;
 
 /**
- * MARKET DATA ADDENDUM EXTENSION RECORD, TYPE 09
+ * MARKET DATA ADDENDUM EXTENSION RECORD, TYPE 10
  * 
  * @author nickfeng
  *
  */
-public class BMarketDataAddendumExtension09 {
+public class BMarketDataAddendumExtension10 {
 
 	/**
-	 * Record Type, 1-2, AN 2, Always 09.
+	 * Record Type, 1-2, AN 2, Always 10.
 	 */
-	private String recordType = "09";
+	private String recordType = "10";
 
 	/**
-	 * Additional Market Data, 3-94, AN 92, Contains the remaining Market Data. Left
+	 * Additional Market Data, 3-18, AN 16, Contains the remaining Market Data. Left
 	 * justified, space filled. Refer to Market Data on page A-8 for a list of valid
 	 * values and Appendix B: Acquirer Reference Data & Market Data for
 	 * explanations.
 	 */
 	private String additionalMarketData;
+
+	/* Filler, 19-94, AN 76, Space filled. */
 
 	/**
 	 * Record Sequence Number, 95-100, N 6, The first record is 000000. Subsequent
@@ -57,18 +59,20 @@ public class BMarketDataAddendumExtension09 {
 	public String toString() {
 		StringBuffer sb = new StringBuffer();
 		sb.append(recordType);
-		sb.append(StringUtils.rightPad(additionalMarketData, 92, ' '));
+		sb.append(StringUtils.rightPad(additionalMarketData, 16, ' '));
+		sb.append(StringUtils.repeat(" ", 76));
 		sb.append(StringUtils.leftPad(recordSequenceNumber, 6, '0'));
 		return sb.toString();
 	}
 
-	public static BMarketDataAddendumExtension09 fromString(String s) {
-		if (StringUtils.isEmpty(s) || s.length() != 100 || !s.startsWith("09")) {
+	public static BMarketDataAddendumExtension10 fromString(String s) {
+		if (StringUtils.isEmpty(s) || s.length() != 100 || !s.startsWith("10")) {
 			return null;
 		}
-		BMarketDataAddendumExtension09 bmb = new BMarketDataAddendumExtension09();
-		bmb.setAdditionalMarketData(s.substring(2, 94));
-		bmb.setRecordSequenceNumber(s.substring(94, 100));
-		return bmb;
+		BMarketDataAddendumExtension10 bmc = new BMarketDataAddendumExtension10();
+		bmc.setAdditionalMarketData(s.substring(2, 18));
+		bmc.setRecordSequenceNumber(s.substring(94, 100));
+		return bmc;
 	}
+
 }
